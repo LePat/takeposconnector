dae4f0925d574ac08ef4bdf736a78ce692b96177
pat <info@lia-concept.fr>
2026-01-17 09:29:02 +0100

    feat: utilisation des websockets pour l'afficheur client et la balance

--- js/takeposconnector.js.php
+++ js/takeposconnector.js.php
@@ -109,6 +109,134 @@ if ($conf->global->{'DIRECTPRINTWHB_SECURE'.$terminaltouse}) $ws = 'wss://';
 
 
 ?>
+function WebSocketSerial(options) {
+    var defaults = {
+        url: 'ws://localhost:12212/serial/DISPLAY',
+        onConnect: function () {
+        },
+        onDisconnect: function () {
+        },
+        onMessage: function (message) {
+        }
+    };
+
+    var settings = Object.assign({}, defaults, options);
+    var websocket;
+    var buffer = '';
+
+    var onMessage = function (evt) {
+        var chr = evt.data;
+        settings.onMessage(chr);
+    };
+
+    var onConnect = function () {
+        settings.onConnect();
+    };
+
+    var onDisconnect = function () {
+        settings.onDisconnect();
+        reconnect();
+    };
+
+    var connect = function () {
+        websocket = new WebSocket(settings.url);
+        websocket.onopen = onConnect;
+        websocket.onclose = onDisconnect;
+        websocket.onmessage = onMessage;
+    };
+
+    var reconnect = function () {
+        connect();
+    };
+
+    this.send = function (message) {
+        websocket.send(message);
+    };
+
+    connect();
+}
+
+
+	const serial = new WebSocketSerial({
+		url: '<?php echo $conf->global->CUSTOMERDISPLAY_WEBSOCKET_URL; ?>'
+	});
+	
+function WebSocketWeigh(options) {
+    var defaults = {
+        url: 'ws://localhost:12212/serial/WEIGH',
+        weightRegex: new RegExp('([0-9]{1,2}\\.[0-9]{3})kg'),
+        stableRegex: new RegExp('^ST.*\\s+'),
+        onConnect: function () {
+        },
+        onDisconnect: function () {
+        },
+        onUpdate: function (weight, stable) {
+
+        }
+    };
+
+    var settings = Object.assign({}, defaults, options);
+    var websocket;
+    var buffer = '';
+
+    var onMessage = function (evt) {
+        var chr = evt.data;
+		console.log("data: " + chr);
+		globalWeight = CheckoutDialog06.parseRecord02(chr).weight;
+		$("#poslines").load("invoice.php?token=<?php echo newToken(); ?>&action=updateqty&place="+place+"&idline="+selectedline+"&number="+globalWeight, function() {
+			editnumber="";
+		});
+		return;
+        if (chr == "\n") {
+            var weightOutput = settings.weightRegex.exec(buffer);
+            var stableOutput = settings.stableRegex.test(buffer);
+
+            if (weightOutput != null) {
+                settings.onUpdate(weightOutput[1], stableOutput);
+            } else {
+				console.log("buffer: " + buffer);
+			}
+            buffer = '';
+        } else {
+            buffer = buffer + chr;
+        }
+		//console.log("buffer: " + buffer);
+    };
+
+    var onConnect = function () {
+        settings.onConnect();
+    };
+
+    var onDisconnect = function () {
+        settings.onDisconnect();
+        reconnect();
+    };
+
+    var connect = function () {
+        websocket = new WebSocket(settings.url);
+        websocket.onopen = onConnect;
+        websocket.onclose = onDisconnect;
+        websocket.onmessage = onMessage;
+    };
+
+    var reconnect = function () {
+        connect();
+    };
+
+    connect();
+    
+    return websocket;
+}
+
+var globalWeight = null;
+
+    var webSocketWeight = WebSocketWeigh({
+        url: '<?php echo $conf->global->WEIGHINGSCALE_WEBSOCKET_URL; ?>',
+        onUpdate: function (weight, stable) {
+            globalWeight = weight;
+            console.log("onUpdate: " + weight + " is stable: " + stable);
+        },
+    });
 
 /* Javascript library of module TakePOS connector */
 
@@ -226,6 +354,7 @@ if (url.includes('/takepos/index.php') ||
 		if (url.includes('/takepos/index.php')) {
 
 			$(document).on('DOMNodeInserted', function (e) {
+				console.log("DOMNodeInserted: " + e);
 				if (e.target.id == "poslines") {
 					//$("#buttonprint").prop("onclick", null).off("click");
 					//$("#buttonprint").unbind();
