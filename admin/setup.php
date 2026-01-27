689573add70bcd21b4b3935ca79a3b2e36341101
pat <info@lia-concept.fr>
2026-01-27 19:21:35 +0100

    feat: possibility to choose template
    - clean needless translations

--- admin/setup.php
+++ admin/setup.php
@@ -112,17 +112,34 @@ $item->fieldAttr['placeholder'] = 'ws://localhost:12212/serial/DISPLAY';
 $item->helpText = 'URL du WebSocket configuré sur le Webapp-Hardware-Bridge pour l\'afficheur client';
 $item->cssClass = 'minwidth500';
 
-for ($indexTerminal = 0; $indexTerminal <= getDolGlobalInt('TAKEPOS_NUM_TERMINALS'); $indexTerminal++) {
-	$formSetup->newItem('Terminal'.$indexTerminal)->setAsTitle();
+// Select printer to use with terminal
+require_once DOL_DOCUMENT_ROOT.'/core/class/dolreceiptprinter.class.php';
+$printer = new dolReceiptPrinter($db);
+$printer->listPrintersTemplates();
+$templates = array();
+foreach ($printer->listprinterstemplates as $key => $value) {
+	$templates[$value['rowid']] = $value['name'];
+}
+
+for ($indexTerminal = 1; $indexTerminal <= getDolGlobalInt('TAKEPOS_NUM_TERMINALS'); $indexTerminal++) {
+	$item = $formSetup->newItem('Terminal'.$indexTerminal)->setAsTitle();
+	$item->nameText = $langs->trans('Terminal').' '.$indexTerminal;
 	$item = $formSetup->newItem('DIRECTPRINTWHB_SECURE'.$indexTerminal);
+	$item->nameText = $langs->trans('DIRECTPRINTWHB_SECURE');
 	$fieldOptions = array(
 		'oui' => $langs->trans('OUI'),
 		'non' => $langs->trans('NON'),
 	);
 	$item->setAsYesNo($fieldOptions);
 	$item = $formSetup->newItem('DIRECTPRINTWHB_IPADDRESS'.$indexTerminal);
+	$item->nameText = $langs->trans('DIRECTPRINTWHB_IPADDRESS');
 	$item = $formSetup->newItem('DIRECTPRINTWHB_PORT'.$indexTerminal);
+	$item->nameText = $langs->trans('DIRECTPRINTWHB_PORT');
 	$item = $formSetup->newItem('DIRECTPRINTWHB_TPPRINTERID'.$indexTerminal);
+	$item->nameText = $langs->trans('DIRECTPRINTWHB_TPPRINTERID');
+	$item = $formSetup->newItem('TAKEPOS_TEMPLATE_TO_USE_FOR_INVOICES'.$indexTerminal);
+	$item->nameText = $langs->trans('TAKEPOS_TEMPLATE_TO_USE_FOR_INVOICES');
+	$item->setAsSelect($templates);
 }
 
 
