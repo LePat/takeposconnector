a8ffc19725ada9540087d960cf19ab1ac832df00
pat <info@lia-concept.fr>
2026-03-04 22:52:15 +0100

    fix: correction du nom des fonction d'impression dans Dolibarr/TakePOS

--- js/takeposconnector.js.php
+++ js/takeposconnector.js.php
@@ -490,7 +490,8 @@ if (url.includes('/takepos/index.php') || url.includes('/compta/facture/card.php
 							button["attributes"]["onclick"].value.includes("TakeposConnector") ||
 							button["attributes"]["onclick"].value.includes("TakeposPrintingOrder") ||
 							button["attributes"]["onclick"].value.includes("TakeposPrinting") ||
-							button["attributes"]["onclick"].value.includes("PrintByESCPOSOld")) {
+							button["attributes"]["onclick"].value.includes("PrintByESCPOSOld") ||
+							button["attributes"]["onclick"].value.includes("PrintByBrowser")) {
 							button["attributes"]["onclick"].value = "DirectPrintWHBDolibarrTakeposPrinting(placeid);";
 						}
 						if (button["attributes"]["onclick"].value.includes("DolibarrOpenDrawer")) {
@@ -520,7 +521,8 @@ if (url.includes('/takepos/index.php') || url.includes('/compta/facture/card.php
 							buttonPrint["attributes"]["onclick"].value.includes("TakeposConnector") ||
 							buttonPrint["attributes"]["onclick"].value.includes("TakeposPrintingOrder") ||
 							buttonPrint["attributes"]["onclick"].value.includes("TakeposPrinting") ||
-							buttonPrint["attributes"]["onclick"].value.includes("PrintByESCPOSOld"))) {
+							buttonPrint["attributes"]["onclick"].value.includes("PrintByESCPOSOld") ||
+							buttonPrint["attributes"]["onclick"].value.includes("PrintByBrowser"))) {
 						buttonPrint["attributes"]["onclick"].value = "DirectPrintWHBDolibarrTakeposPrinting(placeid);";
 						buttonPrint.setAttribute("class", "butAction");
 						buttonPrint.nextElementSibling.setAttribute("class", "butAction");
