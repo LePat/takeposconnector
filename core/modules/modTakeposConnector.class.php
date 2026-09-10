6bee3b599a07c8709add9e24d6406f071c928544
pat <info@lia-concept.fr>
2026-09-10 12:51:51 +0200

    fix: numéro de module dans la plage réservée Lia Concept (192136)
    Ancien numero 449022 tombait dans la plage réservée à TakePOS
    (449000-449999) sur le wiki Dolibarr — sans rapport avec ce module
    depuis qu'il est maintenu de façon indépendante. 192131-192135 étant
    déjà utilisés par d'autres modules Lia Concept, 192136 est le premier
    numéro libre de la plage (192131-192150).

--- core/modules/modTakeposConnector.class.php
+++ core/modules/modTakeposConnector.class.php
@@ -45,7 +45,7 @@ class modTakeposConnector extends DolibarrModules
 
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-		$this->numero = 449022; // TODO Go on page https://wiki.dolibarr.org/index.php/List_of_modules_id to reserve an id number for your module
+		$this->numero = 192136; // Lia Concept range (192131-192150), see https://wiki.dolibarr.org/index.php/List_of_modules_id
 
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'takeposconnector';
